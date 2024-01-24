0a961f024738af3c82b9f418ff1012bb1a3290a5
phamdung2209 <phamdung.22092003@gmail.com>
2024-01-24 07:38:15 +0700

    init project

---
+++ TrainingFPT/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingFPT.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
